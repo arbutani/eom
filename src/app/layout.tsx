ddd4c5fc19446c798d6103684130829de0e88b46
arbutani <butaniankit1107@gmail.com>
2026-09-21 07:11:03 +0530

    google murchent verify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -14,7 +15,11 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Bloom & Curve - Premium Shopping Experience",
-  description: "Discover amazing products with the best shopping experience. Fast delivery, premium quality, and exceptional service.",
+  description:
+    "Discover amazing products with the best shopping experience. Fast delivery, premium quality, and exceptional service.",
+  verification: {
+    google: "es9s0ZeHt4lmd5PXomYKnTf54YYFdvwpfaqNT5JvwvE",
+  },
 };
 
 export default function RootLayout({
@@ -23,8 +28,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
+
